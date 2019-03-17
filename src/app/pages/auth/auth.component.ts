5a1d1ccb9e7af87616300fcb48063963201068f1
ViniciusRomanoBit <romano@alunos.utfpr.edu.br>
2019-03-17 02:39:46 -0300

    senha incorreta modal

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioApi } from '../../../assets/js/services';
-import { LoopBackConfig,LoopBackAuth }        from '../../../assets/js/';
-import {NgForm} from '@angular/forms';
-import { AccessToken }  from '../../../assets/js//models';
-
+import { LoopBackConfig, LoopBackAuth } from '../../../assets/js/';
+import { NgForm } from '@angular/forms';
+import { AccessToken } from '../../../assets/js//models';
+import swal from 'sweetalert2';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -14,17 +14,36 @@ export class AuthComponent implements OnInit {
   toggleFormClass;
   password;
 
-  constructor(private UsuarioApi:UsuarioApi) {
-   }
+  constructor(private UsuarioApi: UsuarioApi) {
+  }
 
   ngOnInit() { }
 
   login(f: NgForm) {    
-    if(f.form.valid){
-      this.UsuarioApi.login({email:f.form.value.email,password:f.form.value.password}).subscribe((response: any) => {
-        LoopBackConfig.setAuthPrefix(response)   
+    if (f.form.valid) {
+      this.UsuarioApi.login({ email: f.form.value.email, password: f.form.value.password }).subscribe(
+        // (response: any) => {
+        // debugger;
+        // LoopBackConfig.setAuthPrefix(response)   
+        // window.location.reload();  
+        // }
+        data => {
+        LoopBackConfig.setAuthPrefix(data)   
         window.location.reload();  
-      });
+        },
+        err => {
+          swal({
+            title: 'Senha incorreta',
+            text: 'Tem certeza de sua senha, apicultor?',
+            imageUrl: '/assets/img/bee-cry.png',
+            imageWidth: 200,
+            imageHeight: 200,   
+            confirmButtonText: 'Voltar',
+
+          })
+          
+        },
+      )
     }
   }
   showSignUp() {
